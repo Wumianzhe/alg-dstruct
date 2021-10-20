afd3d73b89367a7ecb080950345c9b026edb7499
Alexandr Kopnov <a.kopnov24@gmail.com>
2021-10-20 15:36:27 +0300

    format changes

--- 3-Kopnov-B12/src/memallocator.c
+++ 3-Kopnov-B12/src/memallocator.c
@@ -6,29 +6,29 @@
 
 typedef struct memPool {
     int size;
-    void *pMemory;
+    void* pMemory;
 } pool_t;
 
 // keeping struct here so i can do pointer conversion instead of functions
 typedef struct header {
     int prevSize; // also works as offset to make size and pNext adjancent
     int size;
-    struct header *pNext;
+    struct header* pNext;
 } header_t;
 
 #define BLOCK_SIZE 16
 
 pool_t memPool;
-header_t *cur;
+header_t* cur;
 
-bool isMergeable(header_t *this) {
+bool isMergeable(header_t* this) {
     // if next hole is right after this hole
-    return (void *)this->pNext == (void *)this + this->size;
+    return (void*)this->pNext == (void*)this + this->size;
 }
 
-header_t *findPosition(int size) {
+header_t* findPosition(int size) {
     // remember old value of cur
-    header_t *begin = cur;
+    header_t* begin = cur;
     // go for one cycle
     while (cur->pNext != begin) {
         if (cur->pNext->size >= size) {
@@ -49,32 +49,32 @@ int memgetminimumsize() { return 2 * BLOCK_SIZE; }
 
 int memgetblocksize() { return BLOCK_SIZE; }
 
-void merge(header_t *this) {
-    header_t *next = this->pNext;
+void merge(header_t* this) {
+    header_t* next = this->pNext;
     // increase hole size
     this->size += next->size;
     this->pNext = next->pNext;
     // header cleanup
     *next = (header_t){0, 0, 0};
     // change memory on right side
-    *(int *)((void *)this + this->size) = this->size;
+    *(int*)((void*)this + this->size) = this->size;
 }
 
-void *memalloc(int size) {
+void* memalloc(int size) {
     size += BLOCK_SIZE;
-    header_t *header = findPosition(size);
+    header_t* header = findPosition(size);
     if (!header) {
         fprintf(stderr, "Unable to find memory block of sufficient size\n");
         return NULL;
     }
-    header_t *tmp = header;
+    header_t* tmp = header;
     // take care of small leftovers
     if (tmp->size - size < BLOCK_SIZE) {
         size = tmp->size;
     }
     // move header towards end of empty memory
     // type conversion for correct pointer arythmetics
-    header = (void *)header + tmp->size - size;
+    header = (void*)header + tmp->size - size;
     // If header is reserved header
     if (header == memPool.pMemory - 4) {
         fprintf(stderr, "No space remaining\n");
@@ -90,25 +90,23 @@ void *memalloc(int size) {
     header->size = size;
     header->pNext = NULL;
     // prevSize at the end
-    *(int *)((void *)header + size) = -size;
-    // for debug purposes
-    memset((void *)(header + 1), '0', size - BLOCK_SIZE);
+    *(int*)((void*)header + size) = -size;
     // offset returned value
-    return (void *)(header + 1);
+    return (void*)(header + 1);
 }
 
-void memfree(void *p) {
+void memfree(void* p) {
     // offset back
-    header_t *header = (header_t *)(p - BLOCK_SIZE);
+    header_t* header = (header_t*)(p - BLOCK_SIZE);
     // find previous node for merge check
-    header_t *prev;
+    header_t* prev;
     // if previous block is free
     if (header->prevSize > 0) {
-        prev = (void *)header - header->prevSize;
+        prev = (void*)header - header->prevSize;
         // merge
         prev->size += header->size;
         // compiler says assignment to casts is illeagal and one-liner is horrible anyway
-        header_t *next = (void *)header + header->size;
+        header_t* next = (void*)header + header->size;
         next->prevSize = prev->size;
         // try merging with next
         if (isMergeable(prev)) {
@@ -130,7 +128,7 @@ void memfree(void *p) {
     }
 }
 
-int meminit(void *pMemory, int size) {
+int meminit(void* pMemory, int size) {
     if (size < memgetminimumsize()) {
         return false;
     }
@@ -144,11 +142,11 @@ int meminit(void *pMemory, int size) {
 }
 
 void memdone() {
-    void *iter = memPool.pMemory;
+    void* iter = memPool.pMemory;
     while (iter < memPool.pMemory + memPool.size) {
-        if (*(int *)(iter + *(int *)iter - sizeof(int)) < 0) {
-            fprintf(stderr, "MEMORY LEAKED: %d bytes at %p\n", *(int *)iter - 16, iter + 12);
+        if (*(int*)(iter + *(int*)iter - sizeof(int)) < 0) {
+            fprintf(stderr, "MEMORY LEAKED: %d bytes at %p\n", *(int*)iter - 16, iter + 12);
         }
-        iter += *(int *)iter;
+        iter += *(int*)iter;
     }
 }
--- 3-Kopnov-B12/test/func.c
+++ 3-Kopnov-B12/test/func.c
@@ -1,19 +1,20 @@
 #include "memallocator.h"
 #include <criterion/criterion.h>
-#include <criterion/internal/assert.h>
+#include <criterion/redirect.h>
+#include <stdio.h>
 
 char buf[1024];
 struct descriptor {
     int prevSize;
     int size;
-    void *pNext;
+    void* pNext;
 };
 
 Test(basic, allocate_and_free) {
     int poolSize = memgetminimumsize() + 1;
     meminit(&buf, poolSize);
-    void *ptr = memalloc(1);
-    struct descriptor *descr;
+    void* ptr = memalloc(1);
+    struct descriptor* descr;
     cr_expect_not_null(ptr);
     descr = ptr - 16;
     // pNext of allocated node is NULL
@@ -21,24 +22,29 @@ Test(basic, allocate_and_free) {
     // size at beginning is size
     cr_expect_eq(descr->size, memgetblocksize() + 1);
     // size at end is -size
-    cr_expect_eq(*(int *)(ptr + 1), -memgetblocksize() - 1);
-    /* cr_expect_null(memalloc(0)); */
+    cr_expect_eq(*(int*)(ptr + 1), -memgetblocksize() - 1);
+    // can't allocate anything else
+    cr_redirect_stderr();
+    cr_expect_null(memalloc(0));
+    fflush(stderr);
+    cr_expect_stderr_eq_str("No space remaining\n");
+    // freed block tests
     memfree(ptr);
-    descr = (void *)&buf - 4;
+    descr = (void*)&buf - 4;
     cr_expect_eq(descr->pNext, descr);
     // size at beginning is size
     cr_expect_eq(descr->size, poolSize);
     // size at end is size
-    cr_expect_eq(*(int *)(ptr + 1), poolSize);
+    cr_expect_eq(*(int*)(ptr + 1), poolSize);
     memdone();
 }
 
 Test(basic, allocate_and_free_2) {
     int poolSize = memgetminimumsize() + memgetblocksize() + 2;
     meminit(&buf, poolSize);
-    void *ptr1 = memalloc(1);
-    void *ptr2 = memalloc(1);
-    struct descriptor *descr;
+    void* ptr1 = memalloc(1);
+    void* ptr2 = memalloc(1);
+    struct descriptor* descr;
     cr_expect_not_null(ptr1);
     cr_expect_not_null(ptr2);
     descr = ptr1 - 16;
@@ -47,11 +53,11 @@ Test(basic, allocate_and_free_2) {
     // size at beginning is size
     cr_expect_eq(descr->size, memgetblocksize() + 1);
     // size at end is -size
-    cr_expect_eq(*(int *)(ptr1 + 1), -memgetblocksize() - 1);
+    cr_expect_eq(*(int*)(ptr1 + 1), -memgetblocksize() - 1);
     /* cr_expect_null(memalloc(0)); */
     memfree(ptr1);
-    descr = (void *)&buf - 4;
-    struct descriptor *descr2 = ptr1 - 16;
+    descr = (void*)&buf - 4;
+    struct descriptor* descr2 = ptr1 - 16;
     // descriptor added to list
     cr_expect_eq(descr->pNext, descr2);
     // descriptor size is accurate
@@ -63,17 +69,22 @@ Test(basic, allocate_and_free_2) {
 }
 
 Test(basic, merge_right_and_leak) {
+    cr_redirect_stderr();
     int poolSize = sizeof(buf);
     meminit(&buf, poolSize);
-    void *ptr1 = memalloc(1);
-    void *ptr2 = memalloc(1);
-    void *ptr3 = memalloc(1);
+    void* ptr1 = memalloc(1);
+    void* ptr2 = memalloc(1);
+    void* ptr3 = memalloc(1);
     cr_expect(ptr1 && ptr2 && ptr3);
     memfree(ptr1);
     memfree(ptr2);
-    struct descriptor *descr = ptr2 - 16;
-    struct descriptor *head = (void *)&buf - 4;
+    struct descriptor* descr = ptr2 - 16;
+    struct descriptor* head = (void*)&buf - 4;
     cr_expect_eq(head->pNext, descr);
     cr_expect_eq(descr->size, 2 * memgetblocksize() + 2);
     memdone();
+    fflush(stderr);
+    char str[100];
+    sprintf(str, "MEMORY LEAKED: 1 bytes at %p\n", ptr3);
+    cr_expect_stderr_eq_str(str);
 }
